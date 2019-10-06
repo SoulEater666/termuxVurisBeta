6943e524adda96c595ca1c67ad2e2aa65b204775
SoulEater666 <56220236+SoulEater666@users.noreply.github.com>
2019-10-06 11:19:51 +0100

    Add files via upload

---
+++ hacked.py
@@ -0,0 +1,15 @@
+#start
+x = 0
+while x < 190000 :
+ print ("Hacked By SoulEater")
+ x = x+ 1
+
+#stop
+
+if x == 190000 :
+ print ("________________________")
+ print ("                        ")
+ print ("You have to thank me bc i add a stop option :) ")
+ print (" Vuris By SoulEater  V1.0")
+ print ("________________________")
+
